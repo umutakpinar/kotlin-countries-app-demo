b663d5c21cb767ae07f4669c23ff001122b4d01b
Umut Akpinar <umtakpnr5@gmail.com>
2022-11-25 03:21:33 +0300

    Internet permission added now app takes datas from the API

--- app/src/main/java/com/umutakpinar/countriesapp/viewmodel/FeedViewModel.kt
+++ app/src/main/java/com/umutakpinar/countriesapp/viewmodel/FeedViewModel.kt
@@ -3,21 +3,49 @@ package com.umutakpinar.countriesapp.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.umutakpinar.countriesapp.model.Country
+import com.umutakpinar.countriesapp.service.CountryAPIService
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 
 class FeedViewModel : ViewModel(){
+
+    private val countryApiService = CountryAPIService()
+    private val disposable = CompositeDisposable()
+
     val countries = MutableLiveData<List<Country>>()
     val countryError = MutableLiveData<Boolean>()
     val countryLoading = MutableLiveData<Boolean>()
 
     fun refreshData(){
-        val country1 = Country("Turkey","Asia","Ankara","TRY","Turkish","www.something.com")
-        val country2 = Country("France","Europe","Italy","EUR","French","www.something.com")
-        val country3 = Country("Germany","Europe","Berlin","EUR","Deustche","www.something.com")
+        getDataFromAPI()
+    }
 
-        val countryList = arrayListOf<Country>(country1,country2,country3)
+    private fun getDataFromAPI(){
+        countryLoading.value = true
 
-        countries.value = countryList
-        countryError.value = false
-        countryLoading.value = false
+        disposable.add(
+            countryApiService.getData()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<List<Country>>(){
+                    override fun onSuccess(t: List<Country>) {
+                        countryLoading.value = false
+                        countryError.value = false
+                        countries.value = t
+                    }
+
+                    override fun onError(e: Throwable) {
+                        countryLoading.value = false
+                        countryError.value = true
+                        e.printStackTrace() //burada da logcate hatayı bassın varsa
+                    }
+                })
+        )
     }
 }
+
+
+
+
